0a90b825c0f746a46ba3cee2dc9c7a1dabf4e1b5
chanit-ruea <chanit.ruea@bumail.net>
2024-03-20 14:06:42 +0700

    Commit

---
+++ Assets/Airplane.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Airplane : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float enginePowerThrust, liftBooster, drag, angularDrag;
+
+    void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rb.AddForce(transform.forward * enginePowerThrust);
+        }
+
+        Vector3 lift = Vector3.Project(rb.velocity, transform.forward);
+        rb.AddForce(transform.up * lift.magnitude * liftBooster);
+
+        rb.drag = rb.velocity.magnitude * drag;
+        rb.angularDrag = rb.velocity.magnitude * angularDrag;
+
+        rb.AddTorque(Input.GetAxis("Horizontal") * transform.forward * -1);
+        rb.AddTorque(Input.GetAxis("Vertical") * transform.right);
+    }
+}
